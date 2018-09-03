9a13c1a527630cf6c2e2ff80f37b7d6584524e19
Spencer Gauch <spencer.gauch5@gmail.com>
2018-09-03 18:12:51 -0500

    wdv homework page

---
+++ wdv341.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>WDV 341 Homework</title>
+
+<style type = "text/css">
+	
+body {
+	Background-color:white;
+	color:orange;
+	}
+
+h2 {
+	text-align:center;
+	font-size:42px;
+
+	}
+	
+	h3 {
+		text-decoration: underline;
+	}
+
+nav{
+	color:white;
+	background-color: orange;
+	display: block;
+	text-align: center;
+	}
+	
+nav li {
+	display: inline;
+	padding-right: 40px;
+	
+}	
+
+nav a{
+	color: white;
+	text-decoration: none;
+	font-size:22px;
+}
+
+nav a:link {
+	color: white;
+}
+
+nav a:visited {
+	color: white;
+}
+
+nav a:hover {
+    text-decoration: underline;
+	color: white;
+}
+
+.top 
+	{
+	float:right;
+	}
+
+</style>
+
+	
+</head>
+
+<body>
+
+<h2> WDV 341 Finished Homework</h2>
+
+<nav>
+	<ul>
+  		<li><a href = "../WDV101/index.html" title = "home">Home Page</a></li>
+  		<li><a href = "wdv321Homework.html" title = "homework">WDV 321 Homework</a></li>
+  		<li><a href = "../WDV205/wdvhomework.html" title = "homework">WDV 205 Homework</a></li>
+		<li><a href = "../WDV221/jshomework.html" title = "homework">WDV 221 Homework</a></li>
+	</ul>
+</nav>
+
+<h3>JS Review</h3>      
+	<p><a href = "homework/githubDef.html">Github Definitions</a></p>
+
+
+</body>
+</html>
